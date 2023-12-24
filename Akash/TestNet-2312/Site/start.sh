53af9a7b4c18dab024e5ade8c1d175c31a785705
kvas001 <95753493+kvas001@users.noreply.github.com>
2023-12-24 08:31:51 +0500

    Create start.sh

---
+++ Akash/TestNet-2312/Site/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Developed by Dimokus (https://t.me/Dimokus)
+source $HOME/.bashrc
+apt-get install -y sudo nano wget tar zip unzip jq ssh nginx git
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+(echo ${my_root_password}; echo ${my_root_password}) | passwd root
+service ssh restart
+service nginx start
+sleep 5
+rm /var/www/html/index.nginx-debian.html
+cd /var/www/html/
+# git clone https://github.com/PavloDereniuk/Launch-a-website-on-Akash-Network-in-2-minutes .
+git clone https://github.com/kvas001/Tests/Akash/TestNet-2312/Site .
