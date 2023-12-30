ae9eab2a343c32cc83a8defdfe99ae954b23b5b7
kvas001 <95753493+kvas001@users.noreply.github.com>
2023-12-30 10:39:33 +0500

    Update start.sh

--- Akash/nginx-PHP/start.sh
+++ Akash/nginx-PHP/start.sh
@@ -2,13 +2,23 @@
 TZ=Europe/London && ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 echo $OSTYPE
 echo $HOME
-apk add bash ssh git
+echo ${my_root_password}
+# SSH
+# apk add bash openssh
+# rc-update add sshd
+# service sshd start
+# ww
 source $HOME/.bashrc
-apt-get install -y sudo nano wget tar zip unzip jq ssh git
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-(echo ${my_root_password}; echo ${my_root_password}) | passwd root
-service ssh restart
-service nginx start
+# apt-get install -y sudo nano wget tar zip unzip jq ssh git
+RUN apk add --update --no-cache openssh 
+RUN echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config
+# RUN adduser -h /home/root -s /bin/sh -D root
+RUN echo -n 'root:${my_root_password}' | chpasswd
+# ENTRYPOINT ["/entrypoint.sh"]
+EXPOSE 22
+# COPY entrypoint.sh /
+service sshd start
+# service nginx start
 sleep 5
 # rm /var/www/html/index.nginx-debian.html
 # cd /var/www/html/
