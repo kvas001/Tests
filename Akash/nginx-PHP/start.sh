7a5029317eb636ba726eaa3bb1048c13aaf0d076
kvas001 <95753493+kvas001@users.noreply.github.com>
2023-12-30 08:33:45 +0500

    Create start.sh

---
+++ Akash/nginx-PHP/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Developed by Dimokus (https://t.me/Dimokus)
+TZ=Europe/London && ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+source $HOME/.bashrc
+apt-get install -y sudo nano wget tar zip unzip jq ssh git
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+(echo ${my_root_password}; echo ${my_root_password}) | passwd root
+service ssh restart
+service nginx start
+sleep 5
+# rm /var/www/html/index.nginx-debian.html
+# cd /var/www/html/
+# git clone https://github.com/name/repo .
+# git pull
