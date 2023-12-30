20ad08479beb032f34df0dc9daba69206e6ee4d4
kvas001 <95753493+kvas001@users.noreply.github.com>
2023-12-30 12:00:23 +0500

    Update start.sh

--- Akash/nginx-PHP/start.sh
+++ Akash/nginx-PHP/start.sh
@@ -1,24 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 TZ=Europe/London && ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 echo $OSTYPE
 echo $HOME
 echo ${my_root_password}
-# SSH
-# apk add bash openssh
-# rc-update add sshd
-# service sshd start
-apk add --update --no-cache openssh bash git
-source $HOME/.bashrc
 # apt-get install -y sudo nano wget tar zip unzip jq ssh git
+# SSH
+#apk add --update --no-cache openssh bash git
+#source $HOME/.bashrc
+apk add bash
+apk add gcompat libstdc++ curl git
+apk add grep dropbear-scp dropbear-ssh
 echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config
+echo 'AllowTcpForwarding yes' >> /etc/ssh/sshd_config
 # RUN adduser -h /home/root -s /bin/sh -D root
 echo -n 'root:${my_root_password}' | chpasswd
 # ENTRYPOINT ["/entrypoint.sh"]
 # EXPOSE 22
 # COPY entrypoint.sh /
 sleep 5
-rc-update add sshd
-service sshd start
 # service nginx start
 # rm /var/www/html/index.nginx-debian.html
 # cd /var/www/html/
