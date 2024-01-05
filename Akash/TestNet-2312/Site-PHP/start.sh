d93bce0311defba2cdd8c4c5232f7c2dc3e5d804
kvas001 <95753493+kvas001@users.noreply.github.com>
2024-01-05 10:09:16 +0500

    Update start.sh

--- Akash/TestNet-2312/Site-PHP/start.sh
+++ Akash/TestNet-2312/Site-PHP/start.sh
@@ -7,10 +7,16 @@ echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 service ssh restart
 #php
 apt-get install -y php php-cli php-fpm php-json php-common php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath
+mkdir -p /var/run/php
+touch /var/run/php/php7.4-fpm.sock
+service php7.4-fpm stop
+service php7.4-fpm start
+# end php
 service nginx start
 sleep 5
 
-#rm /var/www/html/index.html
+rm /var/www/html/index.nginx-debian.html
+rm /var/www/html/index.html
 cd /var/www/html/
 git clone https://github.com/kvas001/Site-tst .
 ## git pull
